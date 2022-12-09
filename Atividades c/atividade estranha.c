d2a6b1039bc5fe0b6135b7e95dab5dc32451099b
TiagoAlmeida <tiagjudjzihspx2@hotmail.com>
2022-12-09 16:01:36 -0300

    Update atividade estranha.c

--- Atividades c/atividade estranha.c
+++ Atividades c/atividade estranha.c
@@ -16,7 +16,7 @@ struct TPessoa
 int main()
 {
 	
-	int i,op,tam,excluir_codigo,alterar_codigo,apenas_cadastro,j,cont;
+	int i,op,tam,excluir_codigo,alterar_codigo,apenas_cadastro,j,cont,op2,novo_cadastro,aux = 0;
 	
 	setlocale(LC_ALL,"portuguese");
 	
@@ -55,7 +55,7 @@ int main()
 		
 	system("cls");
 	
-	printf("1 - visualizar todos os cadastros\n2 - excluir cadastro\n3 - alterar informaçoes do cadastro\n4 - visualizar apenas um cadastro\n5 - sair do programa\n");
+	printf("1 - visualizar todos os cadastros\n2 - excluir cadastro\n3 - alterar informaçoes do cadastro\n4 - visualizar apenas um cadastro\n5 - novo cadastro\n6 - sair do programa");
 	scanf("%d",&op);
 	
 	switch(op)
@@ -65,8 +65,14 @@ int main()
 			
 			system("cls");
 			
+			if(novo_cadastro != tam && tam != aux && aux > 0)
+			{
+				tam++;
+			}
+			
 			for(i = 0; i < tam; i++)
 			{
+				
 				
 				printf("\n");
 				
@@ -139,9 +145,9 @@ int main()
 				
 			system("cls");
 			
-			printf("alteraçao de dados do cadastro\n");
+			printf("alteraçao de dados do cadastro\n\n");
 			
-			printf("digite o codigo do cadastro:\n");
+			printf("digite o codigo do cadastro que deseja alterar:\n");
 			scanf("%d",&alterar_codigo);
 			
 			for(i = 0; i < tam; i++)
@@ -149,33 +155,127 @@ int main()
 				
 				if(p[i].codigo == alterar_codigo)
 				{
-				
+					op2:
 					system("cls");
+					printf("1 - alterar codigo\n2 - alterar nome\n3 - alterar idade\n4 - alterar sexo\n5 - alterar peso\n6 - alterar altura\n7 - alterar todas as informacoes\n8 - voltar\n");
+					scanf("%d", &op2);
 					
-					printf("alteraçao de dados do cadastro\n");
+					switch(op2)
+					{
+						case 1:
+							
+							system("cls");
 					
-					printf("digite seu novo codigo:");
-					scanf("%d", &p[i].codigo);
+							printf("alteraçao do codigo do cadastro\n");
+							
+							printf("digite seu novo codigo:");
+							scanf("%d", &p[i].codigo);
+							
+							goto op2;
+							
+						break;
+						
+						case 2:
+							
+							system("cls");
 					
-					fflush(stdin);
-					printf("digite seu novo nome:");
-					fgets( p[i].nome, 30,stdin);
+							printf("alteraçao do nome do cadastro\n");
+							
+							fflush(stdin);
+							printf("digite seu novo nome:");
+							fgets( p[i].nome, 30,stdin);
+							
+							goto op2;
+							
+						break;
+						
+						case 3:
+							
+							system("cls");
 					
-					printf("digite sua nova idade:");
-					scanf("%d", &p[i].idade);
+							printf("alteraçao da idade do cadastro\n");
+							
+							printf("digite sua nova idade:");
+							scanf("%d", &p[i].idade);
+							
+							goto op2;
+							
+						break;
+						
+						case 4:
+							
+							system("cls");
 					
-					fflush(stdin);
-					printf("digite seu novo sexo:");
-					fgets(p[i].sexo,30,stdin);
+							printf("alteraçao do sexo do cadastro\n");
+							
+							fflush(stdin);
+							printf("digite seu novo sexo:");
+							fgets(p[i].sexo,30,stdin);
+							
+							goto op2;
+							
+						break;
+						
+						case 5:
+							system("cls");
 					
-					printf("digite seu novo peso:");
-					scanf("%f", &p[i].peso);
+							printf("alteraçao do peso do cadastro\n");
+							
+							printf("digite seu novo peso:");
+							scanf("%f", &p[i].peso);
+							
+							goto op2;
+							
+						break;
+						
+						case 6:
+							system("cls");
 					
-					printf("digite sua nova altura:");
-					scanf("%f", &p[i].altura);
+							printf("alteraçao da altura do cadastro\n");
+							
+							printf("digite sua nova altura:");
+							scanf("%f", &p[i].altura);
+							
+							goto op2;
+							
+						break;
+						
+						case 7:
+							system("cls");
 					
-					goto op;
-			
+							printf("alteraçao de dados do cadastro\n");
+							
+							printf("digite seu novo codigo:");
+							scanf("%d", &p[i].codigo);
+							
+							fflush(stdin);
+							printf("digite seu novo nome:");
+							fgets( p[i].nome, 30,stdin);
+							
+							printf("digite sua nova idade:");
+							scanf("%d", &p[i].idade);
+							
+							fflush(stdin);
+							printf("digite seu novo sexo:");
+							fgets(p[i].sexo,30,stdin);
+							
+							printf("digite seu novo peso:");
+							scanf("%f", &p[i].peso);
+							
+							printf("digite sua nova altura:");
+							scanf("%f", &p[i].altura);
+							
+							goto op2;
+							
+						break;
+						
+						case 8:
+							
+							goto op;
+							
+						break;
+					}
+				
 				}
 				
 			}
@@ -192,7 +292,7 @@ int main()
 			
 			inicio4:
 			
-			printf("digite o codigo do cadastro qe deseja ver\n");
+			printf("digite o codigo do cadastro que deseja ver\n");
 			scanf("%d", &apenas_cadastro);
 			
 			for(i = 0; i < tam; i++)
@@ -224,6 +324,40 @@ int main()
 		break;
 		
 		case 5:
+			
+			aux++;
+			
+			novo_cadastro = tam;
+			
+			system("cls");
+			
+			printf("novo cadastro\n");
+		
+			printf("digite seu codigo:");
+			scanf("%d", &p[tam].codigo);
+			
+			fflush(stdin);
+			printf("digite seu nome:");
+			fgets( p[tam].nome, 30,stdin);
+			
+			printf("digite sua idade:");
+			scanf("%d", &p[tam].idade);
+			
+			fflush(stdin);
+			printf("digite seu sexo:");
+			fgets(p[tam].sexo,30,stdin);
+			
+			printf("digite seu peso:");
+			scanf("%f", &p[tam].peso);
+			
+			printf("digite sua altura:");
+			scanf("%f", &p[tam].altura);
+			
+			goto op;
+			
+		break;
+		
+		case 6:
 			
 		break;
 		
