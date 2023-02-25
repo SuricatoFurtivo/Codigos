4be3406c27fe32bb6343b8d48148a8638cf3ea93
TiagoAlmeida <tiagjudjzihspx2@hotmail.com>
2023-02-25 14:43:27 -0300

    teste

---
+++ Atividades Java/cadastro.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+public class cadastro 
+{
+
+    public static void main()
+    {
+
+        
+
+        JOptionPane.showInputDialog(null,"selecione uma opçao\n\n1 - Cadastrar\n2 - Consultar\n3 - Alterar\n4 - Ordenar Crescente\n5 - Ordenar Decrescente\n6 - Excluir\n7 - Imprimir\n8 - Sair");
+
+    }
+    
+}
